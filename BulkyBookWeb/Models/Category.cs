78a6c1aebc130eed0c8fa69e4aca736b7e4c8aa1
João Carvalho <joao_mf_carvalho@hotmail.com>
2024-10-25 14:52:51 +0100

    Db context configuration

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
