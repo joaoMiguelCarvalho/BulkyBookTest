564debf050c2b8b2191664400d1c6e4048da74bb
João Carvalho <joao_mf_carvalho@hotmail.com>
2024-11-06 17:15:39 +0000

    dropdown menu done and working, and search bar done

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlSer
     //configures the ApplicationDBContext in an ASP.NET Core application to use a SQL Server database (my server is DESKTOP-C79FDSF)
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
